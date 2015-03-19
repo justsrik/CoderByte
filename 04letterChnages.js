db411e53df13f0bc559575ad0711607b94a0dece
Sanga <ssanga2@MTVL13915c38d.local>
2015-03-18 23:55:38 -0700

    4th commit

---
+++ 04letterChnages.js
@@ -0,0 +1,23 @@
+
+function LetterChanges(str) { 
+
+	var letters = "abcdefghijklmnopqrstuvwxyz";
+  var vowels= "aeiou";
+  var newStr="";
+  	for(var i=0;i<str.length;i++){
+    if(str[i] !== "*" && letters.search(str[i]) < 26 && letters.search(str[i]) !== -1 ){
+      if(vowels.search(letters[(letters.search(str[i])+1)]) !== -1){
+    newStr = newStr+letters[(letters.search(str[i])+1)].toUpperCase();
+      }else{
+      newStr = newStr+letters[(letters.search(str[i])+1)];
+      }
+    } else if(str[i] == "*" || letters.search(str[i]) == -1 ){
+    newStr = newStr+str[i];
+    }else{
+    newStr = newStr+letters[0]
+    }
+    }
+  
+  return newStr; 
+         
+}	
