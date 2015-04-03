316810a436240f3c763117dd59d6e3e0940b4310
srikanth <srikanth_sanga@intuit.com>
2015-04-02 22:54:24 -0700

    othercommit

---
+++ checkNums.js
@@ -0,0 +1,13 @@
+function CheckNums(num1,num2) { 
+
+	if(num2>num1){
+    return true;
+    }else if(num2 === num1){
+    return "-1";
+    }else{
+    return false;
+    }
+  
+ // return num1 + num2; 
+         
+}
