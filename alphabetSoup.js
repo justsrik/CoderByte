9a7a1f21f669434b486569033e114cc3b7c175d1
srikanth <srikanth_sanga@intuit.com>
2015-04-10 20:00:36 -0700

    new commit

---
+++ alphabetSoup.js
@@ -0,0 +1,24 @@
+function AlphabetSoup(str) { 
+
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var temp, temp2;
+  var newStr="";
+  var strArr;
+  strArr = str.split("");
+  for(var i=0;i<strArr.length;i++){
+ for(var j=i+1;j<strArr.length-1;j++){
+ if(letters.indexOf(strArr[i])> letters.indexOf(strArr[j])){
+  temp2 = strArr[j];
+   strArr[j] = strArr[i];
+    strArr[i]=temp2;
+   
+    }
+ }
+   
+  }
+  for(var k=0;k<strArr.length;k++){
+  newStr = newStr+strArr[k];
+  }
+  return newStr;
+         
+}
